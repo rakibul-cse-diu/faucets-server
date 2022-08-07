d6b49ea8ef52265c6c7692549f29ada0c4567755
rakibul-cse-diu <rakibul2180@gmail.com>
2022-08-08 02:35:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        console.log("db connected");
+        const usersCollection = client.db('faucets').collection('users');
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World...!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
