f77ed4ec62cefc22f4cbd0546c4259393a11eb4d
rakibul-cse-diu <rakibul2180@gmail.com>
2022-08-09 00:55:01 +0600

    create user routes and schema and incrypt password

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 // import connectDB from "./config/db";
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 require('dotenv').config();
@@ -13,50 +15,10 @@ app.use(express.json());
 
 connectDB();
 
-// const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// async function run() {
-//     try {
-//         await client.connect();
-//         console.log("db connected");
-//         const usersCollection = client.db('faucets').collection('users');
+app.use("/api/users", userRoutes);
 
-//         // user sign up
-//         app.post('/signup', async (req, res) => {
-//             const userData = req.body;
-//             const query = { email: userData.email };
-//             const checkUser = await usersCollection.findOne(query);
-//             if (checkUser === null) {
-//                 const result = await usersCollection.insertOne(userData);
-//                 res.send(result);
-//             } else {
-//                 res.status(400).send({ message: "User already exist" })
-//             }
-//         });
-
-//         // user sign in
-//         app.get('/signin', async (req, res) => {
-//             const userEmail = req.query.email;
-//             const userPassword = req.query.password;
-//             const query = {
-//                 email: userEmail,
-//                 password: userPassword
-//             };
-//             const user = await usersCollection.findOne(query);
-//             if (user !== null) {
-//                 res.send(user);
-//             } else {
-//                 res.status(400).send({ message: "Invalid email or password" })
-//             }
-//         });
-
-//     }
-//     finally {
-
-//     }
-// }
-// run().catch(console.dir);
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send('Hello World...!')
