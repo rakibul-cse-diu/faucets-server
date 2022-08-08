d2d5ce8212162d9cae57bef7bf9451a69e9c5b79
rakibul-cse-diu <rakibul2180@gmail.com>
2022-08-08 23:42:19 +0600

    install mongoose and connect database

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+// import connectDB from "./config/db";
 
 const app = express();
 require('dotenv').config();
@@ -9,49 +11,51 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run() {
-    try {
-        await client.connect();
-        console.log("db connected");
-        const usersCollection = client.db('faucets').collection('users');
+connectDB();
 
-        // user sign up
-        app.post('/signup', async (req, res) => {
-            const userData = req.body;
-            const query = { email: userData.email };
-            const checkUser = await usersCollection.findOne(query);
-            if (checkUser === null) {
-                const result = await usersCollection.insertOne(userData);
-                res.send(result);
-            } else {
-                res.status(400).send({ message: "User already exist" })
-            }
-        });
+// const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// async function run() {
+//     try {
+//         await client.connect();
+//         console.log("db connected");
+//         const usersCollection = client.db('faucets').collection('users');
 
-        // user sign in
-        app.get('/signin', async (req, res) => {
-            const userEmail = req.query.email;
-            const userPassword = req.query.password;
-            const query = {
-                email: userEmail,
-                password: userPassword
-            };
-            const user = await usersCollection.findOne(query);
-            if (user !== null) {
-                res.send(user);
-            } else {
-                res.status(400).send({ message: "Invalid email or password" })
-            }
-        });
+//         // user sign up
+//         app.post('/signup', async (req, res) => {
+//             const userData = req.body;
+//             const query = { email: userData.email };
+//             const checkUser = await usersCollection.findOne(query);
+//             if (checkUser === null) {
+//                 const result = await usersCollection.insertOne(userData);
+//                 res.send(result);
+//             } else {
+//                 res.status(400).send({ message: "User already exist" })
+//             }
+//         });
 
-    }
-    finally {
+//         // user sign in
+//         app.get('/signin', async (req, res) => {
+//             const userEmail = req.query.email;
+//             const userPassword = req.query.password;
+//             const query = {
+//                 email: userEmail,
+//                 password: userPassword
+//             };
+//             const user = await usersCollection.findOne(query);
+//             if (user !== null) {
+//                 res.send(user);
+//             } else {
+//                 res.status(400).send({ message: "Invalid email or password" })
+//             }
+//         });
 
-    }
-}
-run().catch(console.dir);
+//     }
+//     finally {
+
+//     }
+// }
+// run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
